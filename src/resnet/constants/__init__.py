f429a22b4fb9296c3c9daab04b4fe73693b90895
Quik Zi Hao <94067315+QuikZiHao@users.noreply.github.com>
2024-12-25 01:42:52 +0800

    init const

---
+++ src/resnet/constants/__init__.py
@@ -0,0 +1,16 @@
+import os 
+
+DATASET_DIR = os.path.join("assets", "dataset")
+DATACLASS = {
+    0:"anon",
+    1:"tomori",
+    2:"taki",
+    3:"soyo",
+    4:"rana",
+    5:"saki",
+    6:"muzimi",
+    7:"umirin",
+    8:"ueka",
+    9:"nyamuchi",
+    10:"other"
+}
